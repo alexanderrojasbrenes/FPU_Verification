563fb156a06d830b289645df9c0898c42d334d35
Alexander <ALEXANDER.ROJASBRENES@ucr.ac.cr>
2024-05-12 19:43:20 -0600

    Se agregó la posibilidad de producir delays aleatorios (pero acotados) entrecada operación ejecutada por la FPU.

--- test_1.sv
+++ test_1.sv
@@ -1,11 +1,48 @@
+// Test case
 program testcase(intf_cnt intf);
   environment env = new(intf);
-         
-  initial
-    begin
+  int random_delay; // Variable para almacenar el retardo aleatorio
+  int iterations; // Variable para almacenar el número de iteraciones de cada operación
+  
+  initial begin
     env.drvr.reset();
-      env.drvr.addition(2);
     
-      #100; $finish;
+    iterations = 50; // Cantidad de iteraciones para cada operación
+    
+    // Suma
+    repeat (iterations) begin
+      env.drvr.addition(); // Ejecuta la suma una vez
+      random_delay = $urandom_range(0, 50); // Retardo aleatorio entre iteraciones
+      #random_delay;
     end
+
+    /*
+    // Resta
+    repeat (iterations) begin
+      env.drvr.substraction(); // Ejecuta la resta una vez
+      random_delay = $urandom_range(0, 50);
+      #random_delay;
+    end
+    
+    
+    // Multiplicación
+    repeat (iterations) begin
+      env.drvr.multiplication(); // Ejecuta la multiplicación una vez
+      random_delay = $urandom_range(0, 50);
+      #random_delay;
+    end
+    
+    
+    // División
+    repeat (iterations) begin
+      env.drvr.division(); // Ejecuta la división una vez
+      random_delay = $urandom_range(0, 50);
+      #random_delay;
+    end
+    */
+    
+    
+    #100; // Tiempo de espera final antes de finalizar la simulación
+    $finish;
+  end
 endprogram
