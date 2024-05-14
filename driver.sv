54cf096b9b61079e6d9baf006eaf993469214dfb
Alexander <ALEXANDER.ROJASBRENES@ucr.ac.cr>
2024-05-13 21:25:13 -0600

    Se randomizan las iteraciones, delays y la selección de operaciones.

--- driver.sv
+++ driver.sv
@@ -9,7 +9,7 @@ class driver;
     this.sb = sb;
   endfunction
         
-  task reset(); // Reset method
+  task reset();  // Reset method
     $display("Executing reset\n");
     @ (posedge intf.clk);
     intf.opa = 0;
@@ -17,6 +17,7 @@ class driver;
     intf.rmode = 0;
     intf.fpu_op = 0;
   endtask
+  
   
   task addition();
       sti = new();
@@ -33,8 +34,10 @@ class driver;
         intf.opa = sti.valueA;
         intf.opb = sti.valueB;
       
-      sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) + $bitstoshortreal(sti.valueB))); // Se guarda floatA + floatB en sb, pero en su representacion de IEEE 754
+      // Se guarda floatA + floatB en sb, pero en su representacion de IEEE 754
+      sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) + $bitstoshortreal(sti.valueB))); 
   endtask
+  
   
   task substraction();
       sti = new();
@@ -54,6 +57,7 @@ class driver;
         sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) - $bitstoshortreal(sti.valueB)));
   endtask
   
+  
   task multiplication();
       sti = new();
       @ (posedge intf.clk); 
@@ -72,6 +76,7 @@ class driver;
         sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) * $bitstoshortreal(sti.valueB)));
   endtask
       
+  
   task division();
         sti = new();
         @ (posedge intf.clk); 
@@ -89,5 +94,38 @@ class driver;
         intf.opb = sti.valueB; 
         sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) / $bitstoshortreal(sti.valueB)));
   endtask
-
+  
+  
+  task random_operations();
+        sti = new();
+        @ (posedge intf.clk); 
+        if(sti.randomize()) 
+        
+       	for (int i; i < sti.iterations; i++) //i hasta el número de iteraciones que se escogió
+        begin
+          if(sti.operation_list[i] == 0) // suma
+          begin
+            addition();
+          	#sti.random_delay;
+          end
+          
+          else if(sti.operation_list[i] == 1) // resta
+          begin
+            substraction(); 
+          	#sti.random_delay;
+          end
+          
+          else if(sti.operation_list[i] == 2) // multiplicación
+          begin
+            multiplication(); 
+          	#sti.random_delay;
+          end
+          
+          else if(sti.operation_list[i] == 3) // división
+          begin
+            division(); 
+          	#sti.random_delay;
+          end            
+        end
+  endtask 
 endclass
--- test_1.sv
+++ test_1.sv
@@ -7,40 +7,7 @@ program testcase(intf_cnt intf);
   initial begin
     env.drvr.reset();
     
-    iterations = 50; // Cantidad de iteraciones para cada operación
-    
-    // Suma
-    repeat (iterations) begin
-      env.drvr.addition(); // Ejecuta la suma una vez
-      random_delay = $urandom_range(0, 50); // Retardo aleatorio entre iteraciones
-      #random_delay;
-    end
-
-    /*
-    // Resta
-    repeat (iterations) begin
-      env.drvr.substraction(); // Ejecuta la resta una vez
-      random_delay = $urandom_range(0, 50);
-      #random_delay;
-    end
-    
-    
-    // Multiplicación
-    repeat (iterations) begin
-      env.drvr.multiplication(); // Ejecuta la multiplicación una vez
-      random_delay = $urandom_range(0, 50);
-      #random_delay;
-    end
-    
-    
-    // División
-    repeat (iterations) begin
-      env.drvr.division(); // Ejecuta la división una vez
-      random_delay = $urandom_range(0, 50);
-      #random_delay;
-    end
-    */
-    
+    env.drvr.random_operations(); 
     
     #100; // Tiempo de espera final antes de finalizar la simulación
     $finish;
