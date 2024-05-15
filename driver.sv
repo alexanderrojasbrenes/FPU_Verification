bca6718481b615e2730253ecc81f5686b3433714
Alexander <ALEXANDER.ROJASBRENES@ucr.ac.cr>
2024-05-15 01:16:26 -0600

    Se agregó el task 'extreme_cases', el cual por ahora se encarga de recibir operandos que producen indefiniciones en las operaciones y probar si el resultado es adecuado. Tambi[en se modificó la estructura de los task de cada una de las operaciones de forma que a través de parámetros, fuera más simple ingresar los estímulos.

--- driver.sv
+++ driver.sv
@@ -18,144 +18,127 @@ class driver;
     intf.fpu_op = 0;
   endtask
   
-  
-  task addition();
-      sti = new();
-      @ (posedge intf.clk); // sincronizacion, no es un  "always"
-      if(sti.randomize()) // Generate stimulus Se pone 'if' por que el randomize puede fallar si tiene constraints que no cumple
-        $display("Executing addition\n");
-        $display("Driving 0x%d valueA in the DUT\n", sti.valueA[30:23]);
-        $display("	as float: %f valueA in the DUT\n", $bitstoshortreal(sti.valueA)); // convierte el valorA de 32 bits a un flotante
-        $display("Driving 0x%d valueB in the DUT\n", sti.valueB[30:23]);
-      	$display("	as float: %f valueB in the DUT\n", $bitstoshortreal(sti.valueB));
-        
-        intf.rmode = 0;
-       	intf.fpu_op = 0;
-        intf.opa = sti.valueA;
-        intf.opb = sti.valueB;
+  task addition(logic [31:0] valueA, logic [31:0] valueB);
+    sti = new();
+    @ (posedge intf.clk); // sincronizacion, no es un  "always"
+    if(sti.randomize()) // Generate stimulus: Se pone 'if' por que el randomize puede fallar si tiene constraints que no cumple
+      $display("Executing addition\n");
+      $display("Driving valueA: %f in the DUT\n", $bitstoshortreal(valueA));
+      $display("Driving valueB: %f in the DUT\n", $bitstoshortreal(valueB)); 
       
+      intf.rmode = 0;
+      intf.fpu_op = 0;
+      intf.opa = valueA;
+      intf.opb = valueB;
+    
       // Se guarda floatA + floatB en sb, pero en su representacion de IEEE 754
-      sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) + $bitstoshortreal(sti.valueB))); 
+      sb.store.push_front($shortrealtobits($bitstoshortreal(valueA) + $bitstoshortreal(valueB))); 
   endtask
   
+  task substraction(logic [31:0] valueA, logic [31:0] valueB);
+    sti = new();
+    @ (posedge intf.clk); 
+    if(sti.randomize()) 
+      $display("Executing substraction\n");
+      $display("Driving valueA: %f in the DUT\n", $bitstoshortreal(valueA));
+      $display("Driving valueB: %f in the DUT\n", $bitstoshortreal(valueB));
   
-  task substraction();
-      sti = new();
-      @ (posedge intf.clk); 
-      if(sti.randomize()) 
-        $display("Executing substraction\n");
-      	$display("Driving 0x%d valueA in the DUT\n", sti.valueA[30:23]);
-        $display("	as float: %f valueA in the DUT\n", $bitstoshortreal(sti.valueA));
-
-        $display("Driving 0x%d valueB in the DUT\n", sti.valueB[30:23]);
-      	$display("	as float: %f valueB in the DUT\n", $bitstoshortreal(sti.valueB));
-
-        intf.rmode = 0;
-        intf.fpu_op = 1;
-        intf.opa = sti.valueA; 
-        intf.opb = sti.valueB; 
-        sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) - $bitstoshortreal(sti.valueB)));
+      intf.rmode = 0;
+      intf.fpu_op = 1;
+      intf.opa = valueA;
+      intf.opb = valueB;
+      sb.store.push_front($shortrealtobits($bitstoshortreal(valueA) - $bitstoshortreal(valueB)));
   endtask
   
+  task multiplication(logic [31:0] valueA, logic [31:0] valueB);
+    sti = new();
+    @ (posedge intf.clk); 
+    if(sti.randomize()) 
+      $display("Executing multiplication\n");
+      $display("Driving valueA: %f in the DUT\n", $bitstoshortreal(valueA));
+      $display("Driving valueB: %f in the DUT\n", $bitstoshortreal(valueB));
+
+      intf.rmode = 0;
+      intf.fpu_op = 2;
+      intf.opa = valueA; 
+      intf.opb = valueB; 
+      sb.store.push_front($shortrealtobits($bitstoshortreal(valueA) * $bitstoshortreal(valueB)));
+  endtask  
   
-  task multiplication();
-      sti = new();
-      @ (posedge intf.clk); 
-      if(sti.randomize()) 
-        $display("Executing multiplication\n");
-      	$display("Driving 0x%d valueA in the DUT\n", sti.valueA[30:23]);
-        $display("	as float: %f valueA in the DUT\n", $bitstoshortreal(sti.valueA));
+  task division(logic [31:0] valueA, logic [31:0] valueB);
+    sti = new();
+    @ (posedge intf.clk); 
+    if(sti.randomize()) 
+    $display("Executing multiplication\n");
+    $display("Driving valueA: %f in the DUT\n", $bitstoshortreal(valueA));
+    $display("Driving valueB: %f in the DUT\n", $bitstoshortreal(valueB));
 
-        $display("Driving 0x%d valueB in the DUT\n", sti.valueB[30:23]);
-      	$display("	as float: %f valueB in the DUT\n", $bitstoshortreal(sti.valueB));
-
-        intf.rmode = 0;
-      	intf.fpu_op = 2;
-        intf.opa = sti.valueA; 
-        intf.opb = sti.valueB; 
-        sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) * $bitstoshortreal(sti.valueB)));
+    intf.rmode = 0;
+    intf.fpu_op = 3;
+    intf.opa = valueA; 
+    intf.opb = valueB; 
+    sb.store.push_front($shortrealtobits($bitstoshortreal(valueA) / $bitstoshortreal(valueB)));
   endtask
-      
-  
-  task division();
-        sti = new();
-        @ (posedge intf.clk); 
-        if(sti.randomize()) 
-        $display("Executing multiplication\n");
-      	$display("Driving 0x%d valueA in the DUT\n", sti.valueA[30:23]);
-        $display("	as float: %f valueA in the DUT\n", $bitstoshortreal(sti.valueA));
-
-        $display("Driving 0x%d valueB in the DUT\n", sti.valueB[30:23]);
-      	$display("	as float: %f valueB in the DUT\n", $bitstoshortreal(sti.valueB));
-
-        intf.rmode = 0;
-      	intf.fpu_op = 3;
-        intf.opa = sti.valueA; 
-        intf.opb = sti.valueB; 
-        sb.store.push_front($shortrealtobits($bitstoshortreal(sti.valueA) / $bitstoshortreal(sti.valueB)));
-  endtask
-  
   
   task random_operations();
-        sti = new();
-        @ (posedge intf.clk); 
-        if(sti.randomize()) 
-        
-       	for (int i; i < sti.iterations; i++) //i hasta el número de iteraciones que se escogió
-        begin
-          if(sti.operation_list[i] == 0) // suma
-          begin
-            addition();
-          	#sti.random_delay;
-          end
-          
-          else if(sti.operation_list[i] == 1) // resta
-          begin
-            substraction(); 
-          	#sti.random_delay;
-          end
-          
-          else if(sti.operation_list[i] == 2) // multiplicación
-          begin
-            multiplication(); 
-          	#sti.random_delay;
-          end
-          
-          else if(sti.operation_list[i] == 3) // división
-          begin
-            division(); 
-          	#sti.random_delay;
-          end            
-        end
-  endtask 
-
-  task extreme_cases(input integer op, input logic valueA, input logic valueB);
-      sti = new();
-      @ (posedge intf.clk); 
-      if(sti.randomize()) 
-        if(op == 0) // suma
-          begin
-            addition();
-            #sti.random_delay;
-          end
-            
-      else if(op == 1) // resta
-            begin
-              substraction(); 
-              #sti.random_delay;
-            end
-            
-      else if(op == 2) // multiplicación
-            begin
-              multiplication(); 
-              #sti.random_delay;
-            end
-            
-      else if(op == 3) // división
-            begin
-              division(); 
-          #sti.random_delay;
-        end
+    sti = new();
+    @ (posedge intf.clk); 
+    if(sti.randomize()) 
       
-endtask 
+    for (int i; i < sti.iterations; i++) //i hasta el número de iteraciones que se escogió
+    begin
+      if(sti.operation_list[i] == 0) // suma
+      begin
+        addition(sti.valueA, sti.valueB);
+        #sti.random_delay;
+      end
+      
+      else if(sti.operation_list[i] == 1) // resta
+      begin
+        substraction(sti.valueA, sti.valueB); 
+        #sti.random_delay;
+      end
+      
+      else if(sti.operation_list[i] == 2) // multiplicación
+      begin
+        multiplication(sti.valueA, sti.valueB); 
+        #sti.random_delay;
+      end
+      
+      else if(sti.operation_list[i] == 3) // división
+      begin
+        division(sti.valueA, sti.valueB); 
+        #sti.random_delay;
+      end  
+    end
+  endtask 
+  
+  task extreme_cases(integer op);
+    sti = new();
+    @ (posedge intf.clk); 
+    if(sti.randomize()) 
+      if(op == 0) // suma
+      begin
+        addition(sti.p_inf, sti.p_inf);
+        #sti.random_delay;
+      end
+        
+      else if(op == 1) // resta
+      begin
+        substraction(sti.n_inf, sti.p_inf); 
+        #sti.random_delay;
+      end
+        
+      else if(op == 2) // multiplicación
+      begin
+        multiplication(sti.valueA, sti.p_inf); 
+        #sti.random_delay;
+      end
+        
+      else if(op == 3) // división
+      begin
+        division(sti.valueA, sti.p_inf); 
+        #sti.random_delay;
+      end        	
+  endtask
 endclass
