e85a0175735071f2372aacda534ebaeaf2893a0a
Alexander <ALEXANDER.ROJASBRENES@ucr.ac.cr>
2024-05-15 01:20:53 -0600

    En el primer test, se maneja el funcionamiento 'normal' de la FPU, utilizando operandos aleatorios y en segundo se hacen pruebas 'a pie' de tres casos extremos inducidos.

--- test_1.sv
+++ test_1.sv
@@ -1,13 +1,11 @@
 // Test case
 program testcase(intf_cnt intf);
   environment env = new(intf);
-  int random_delay; // Variable para almacenar el retardo aleatorio
-  int iterations; // Variable para almacenar el número de iteraciones de cada operación
   
   initial begin
     env.drvr.reset();
     
-    env.drvr.random_operations(); 
+    env.drvr.random_operations();
     
     #100; // Tiempo de espera final antes de finalizar la simulación
     $finish;
